1216b1d8b45da8cb8a8fe7bfc6230c3c72965d59
mansi Kumari <mannsi980k@gmail.com>
2025-04-06 19:54:32 +0530

    updated column

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ function Home() {
     <div className="cover">
       <h1>Welcome To React Tutorials</h1>
       <Link to="tut1">Click to view React Tut 1</Link>
+      <br></br>
       <Link to="tut2">Click to view React Tut 2</Link>
 
     </div>
