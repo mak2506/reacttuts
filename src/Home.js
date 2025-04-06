47dc02971f801cd3f5d6439b33639e426a8f970c
mansi Kumari <mannsi980k@gmail.com>
2025-04-06 19:43:29 +0530

    added routing

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import './App.css';
+
+function Home() {
+  return (
+    <div className="cover">
+      <h1>Welcome To React Tutorials</h1>
+      <Link to="tut1">Click to view React Tut 1</Link>
+      <Link to="tut2">Click to view React Tut 2</Link>
+
+    </div>
+  );
+}
+
+export default Home;
